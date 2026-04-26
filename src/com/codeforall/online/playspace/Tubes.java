a6f0d704ddd56a11bf2b6b5215ae6f2c2abb41cb
nunov2 <nuno.a.c.vieira@gmail.com>
2026-04-26 23:15:19 +0100

    Refactored Tubes class to remove duplicate code

--- src/com/codeforall/online/playspace/Tubes.java
+++ src/com/codeforall/online/playspace/Tubes.java
@@ -7,162 +7,112 @@ import com.codeforall.simplegraphics.graphics.Rectangle;
 import com.codeforall.simplegraphics.pictures.Picture;
 
 public class Tubes {
-    private UpperTube upperTube;
-    private LowerTube lowerTube;
-    private boolean passed;
+    private boolean passed, goingUp;
     private int spawnTubesX, spawnTubesY;
+    private Rectangle upperHitbox, lowerHitbox;
+    private Picture upperTube, lowerTube;
 
     public void spawnTubes(int startingX, int startingY) {
-        upperTube = new UpperTube(startingX, startingY); // min y: -800
-        lowerTube = new LowerTube(startingX, startingY + 1200);// Max y - 1060
         spawnTubesX = startingX;
         spawnTubesY = startingY;
+
+        upperTube = new Picture(startingX - 20, startingY, Main.PREFIX + "tube_upper.png");
+        upperTube.draw();
+
+        upperHitbox = new Rectangle(startingX,startingY, 100,1000);
+        upperHitbox.setColor(Color.ORANGE);
+        upperHitbox.draw();
+
+        lowerTube = new Picture(startingX - 20, startingY + 1200, Main.PREFIX + "tube_lower.png");
+        lowerTube.draw();
+
+        lowerHitbox = new Rectangle(startingX, startingY + 1200, 100,1000);
+        lowerHitbox.setColor(Color.ORANGE);
+        lowerHitbox.draw();
     }
 
     public void restartPosition() {
-        upperTube.resetPosition(spawnTubesX ,spawnTubesY);
-        lowerTube.resetPosition(spawnTubesX,(spawnTubesY + 1200));
+        resetPosition(spawnTubesX ,spawnTubesY);
     }
 
-    public void constantMove() {
-        upperTube.constantMove();
-        lowerTube.constantMove();
+    public void move() {
+        upperHitbox.translate(-3 , 0);
+        lowerHitbox.translate(-3 , 0);
+        upperTube.translate(-3, 0);
+        lowerTube.translate(-3, 0);
     }
 
-    class UpperTube {
-        private Rectangle hitbox;
-        private Picture image;
-        private boolean goingUp;
+    public void resetPosition(int x, int y) {
+        int dxUpper = x - upperHitbox.getX();
+        int dyUpper = y - upperHitbox.getY();
+        int dxLower = x - lowerHitbox.getX();
+        int dyLower = y - lowerHitbox.getY();
 
-        public UpperTube(int startingX, int startingY) {
+        upperHitbox.translate(dxUpper, dyUpper);
+        upperTube.translate(dxUpper, dyUpper);
+        lowerHitbox.translate(dxLower, dyLower + 1200);
+        lowerTube.translate(dxLower, dyLower + 1200);
 
-            image = new Picture(startingX - 20, startingY, Main.PREFIX + "tube_upper.png");
-            image.draw();
-
-            hitbox = new Rectangle(startingX,startingY, 100,1000);
-            hitbox.setColor(Color.ORANGE);
-        }
-
-        public void move() {
-            hitbox.translate(-3 , 0);
-            image.translate(-3, 0);
-        }
-
-        public void constantMove() {
-                if (getUpperY() >= -300) {
-                    goingUp = false;
-                }
-                else if (getUpperY() <= -800) {
-                    goingUp = true;
-                }
-                if (!goingUp) {
-                    hitbox.translate(0, -1);
-                    image.translate(0, -1);
-                }
-                else {
-                    hitbox.translate(0, 1);
-                    image.translate(0, 1);
-                }
-
-        }
-
-        public void resetPosition(int x, int y) {
-            int dx = x - hitbox.getX();
-            int dy = y - hitbox.getY();
-
-            hitbox.translate(dx, dy);
-            image.translate(dx, dy);
-        }
-
-    }
-
-    class LowerTube {
-        private Rectangle hitbox;
-        private Picture image;
-        private boolean goingUp;
-
-        public LowerTube(int startingX, int startingY) {
-
-            image = new Picture(startingX - 20, startingY, Main.PREFIX + "tube_lower.png");
-            image.draw();
-
-            hitbox = new Rectangle(startingX, startingY, 100,1000);
-            hitbox.setColor(Color.ORANGE);
-        }
-
-        public void move() {
-            hitbox.translate(-3 , 0);
-            image.translate(-3, 0);
-        }
-
-        public void resetPosition(int x, int y) {
-            int dx = x - hitbox.getX();
-            int dy = y - hitbox.getY();
-
-            hitbox.translate(dx, dy);
-            image.translate(dx, dy);
-
-            passed = false;
-        }
-
-        public void constantMove() {
-            if (getUpperY() == -300) {
-                goingUp = false;
-            }
-            else if (getUpperY() == -800) {
-                goingUp = true;
-            }
-            if (!goingUp) {
-                hitbox.translate(0, -1);
-                image.translate(0, -1);
-            }
-            else {
-                hitbox.translate(0, 1);
-                image.translate(0, 1);
-            }
-
-        }
-
+        passed = false;
     }
 
     public void moveAll() {
-        if (upperTube != null && lowerTube != null) {
-            upperTube.move();
-            lowerTube.move();
-        }
 
-        if (upperTube.hitbox.getX() + upperTube.hitbox.getWidth() + 40 < 0) {
+        move();
+
+        if (upperHitbox.getX() + upperHitbox.getWidth() + 40 < 0) {
             int newX = 1060;
             int newUpperY = Random.randomInt(-800, -300);
             int newLowerY = newUpperY + 1200;
 
-            upperTube.resetPosition(newX, newUpperY);
-            lowerTube.resetPosition(newX, newLowerY);
+            resetPosition(newX, newUpperY);
+        }
+
+    }
+
+    public void constantMove() {
+        if (getUpperY() >= -300) {
+            goingUp = false;
+        }
+        else if (getUpperY() <= -800) {
+            goingUp = true;
+        }
+        if (!goingUp) {
+            upperHitbox.translate(0, -1);
+            upperTube.translate(0, -1);
+            lowerHitbox.translate(0, -1);
+            lowerTube.translate(0, -1);
+        }
+        else {
+            upperHitbox.translate(0, 1);
+            upperTube.translate(0, 1);
+            lowerHitbox.translate(0, 1);
+            lowerTube.translate(0, 1);
         }
 
     }
 
     public int getUpperWidth() {
-        return upperTube.hitbox.getWidth();
+        return upperHitbox.getWidth();
     }
-    public int getUpperHeight() { return upperTube.hitbox.getHeight(); }
+    public int getUpperHeight() { return upperHitbox.getHeight(); }
     public int getUpperX() {
-        return upperTube.hitbox.getX();
+        return upperHitbox.getX();
     }
     public int getUpperY() {
-        return upperTube.hitbox.getY();
+        return upperHitbox.getY();
     }
 
 
     public int getLowerWidth() {
-        return lowerTube.hitbox.getWidth();
+        return lowerHitbox.getWidth();
     }
-    public int getLowerHeight() { return lowerTube.hitbox.getHeight(); }
+    public int getLowerHeight() { return lowerHitbox.getHeight(); }
     public int getLowerX() {
-        return lowerTube.hitbox.getX();
+        return lowerHitbox.getX();
     }
     public int getLowerY() {
-        return lowerTube.hitbox.getY();
+        return lowerHitbox.getY();
     }
 
     public void setPassed() {
